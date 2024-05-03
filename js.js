8bd2da48bfcffcb2712b26e41131b425e581d512
sranack <113941315+sranack@users.noreply.github.com>
2024-05-03 09:13:04 -0300

    Update js.js
    lgl

--- js.js
+++ js.js
@@ -5,7 +5,7 @@ var id = 0;
 $.ajax(url)
     .done(function(xml){
         $(xml).find("pirata").each(function(){
-            $("#cards").append('<div class="card"><a href="individual.html?id='+$(this).find("id").text()+'"><p class="procurado">Procurado</p> <img class="foto"src="'+ $(this).find("id").text() +'.png"><p class="vivo">Vivo ou Morto</p> <p class="nome">'+ $(this).find("nome").text() +'</p> <p>'+ $(this).find("alcunha").text()+'<\p> <p class="rec"> <img class="berrys" src="berrys.png">'+ $(this).find("recompensa").text()+' </p><p> Status:'+ $(this).find("status").text()+'</a> </p><\div>');
+            $("#cards").append('<div class="card"><a href="individual.html?id='+$(this).find("id").text()+'">  <img class="foto"src="'+ $(this).find("id").text() +'.png"> <p class="nome">'+ $(this).find("nome").text() +'</p> <p>'+ $(this).find("alcunha").text()+'<\p> <p class="rec"> <img class="berrys" src="berrys.png">'+ $(this).find("recompensa").text()+' </p><p> Status:'+ $(this).find("status").text()+'</a> </p><\div>');
         });
     })
     .fail(function(){
@@ -24,7 +24,7 @@ $.ajax(url)
             $(xml).find("pirata").each(function(){
                 var pos = parseInt($(this).find("id").text())
                 if(id == pos){
-                $("#individual").append('<div class="card"><p class="procurado">Procurado</p> <img class="foto"src="'+ $(this).find("id").text() +'.png"><p class="vivo">Vivo ou Morto</p> <p class="nome">'+ $(this).find("nome").text() +'</p> <p>'+ $(this).find("alcunha").text()+'<\p> <p class="rec"> <img class="berrys" src="berrys.png">'+ $(this).find("recompensa").text()+' </p><p> Status:'+ $(this).find("status").text()+' </p><\div>');
+                $("#individual").append('<div class="card"> <img class="foto"src="'+ $(this).find("id").text() +'.png"><p class="nome">'+ $(this).find("nome").text() +'</p> <p>'+ $(this).find("alcunha").text()+'<\p> <p class="rec"> <img class="berrys" src="berrys.png">'+ $(this).find("recompensa").text()+' </p><p> Status:'+ $(this).find("status").text()+' </p><\div>');
                 }
             });
         
